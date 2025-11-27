7b29fd1a09e3b565ee690ddcede022520a35a6fe
abinayaramanathan <rabinayasep@gmail.com>
2025-11-27 17:24:33 +0530

    android update bug fix

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -10,19 +10,19 @@ router.get('/version', (_req, res) => {
     // TODO: Update these values when releasing a new version
     // You can also read from a config file or environment variables
     const versionInfo = {
-      version: '1.0.7',  // Update this when releasing new version
-      buildNumber: '8',  // Update this when releasing new version
+      version: '1.0.8',  // Update this when releasing new version
+      buildNumber: '9',  // Update this when releasing new version
       platforms: {
         windows: {
-          downloadUrl: 'https://github.com/Abinaya-Ramanathan/central360/releases/download/v1.0.7/Company360-Setup.exe',
+          downloadUrl: 'https://github.com/Abinaya-Ramanathan/central360/releases/download/v1.0.8/Company360-Setup.exe',
           isRequired: false,
         },
         android: {
-          downloadUrl: 'https://github.com/Abinaya-Ramanathan/central360/releases/download/v1.0.7/company360-v1.0.7.apk',
+          downloadUrl: 'https://github.com/Abinaya-Ramanathan/central360/releases/download/v1.0.8/company360-v1.0.8.apk',
           isRequired: false,
         }
       },
-      releaseNotes: 'Added Sales and Credit Details, Production and Expense Details, Attendance and Advance Details, black Android icon background, code refactoring',
+      releaseNotes: 'Fixed Android APK installation prompt issue, added Sales and Credit Details, Production and Expense Details, Attendance and Advance Details, black Android icon background, code refactoring',
       isRequired: false,  // Set to true for critical security updates
       releaseDate: '2025-12-01T00:00:00Z',
     };
